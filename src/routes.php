077419d8e94aefcb4145eecf4d17f237eb778824
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2014-06-05 11:24:14 -0400

    Reformat routes.php.

--- src/routes.php
+++ src/routes.php
@@ -1,9 +1,10 @@
 <?php
 return function(\Slim\Slim $app) {
-    $app->get('/video', function() use($app) {
-        $app->render('video.html', ['page' => 'video']);
-    })->name('video');
     $app->get('/', function() use($app) {
         $app->render('home.html', ['page' => 'home']);
     })->name('home');
+
+    $app->get('/video', function() use($app) {
+        $app->render('video.html', ['page' => 'video']);
+    })->name('video');
 };
