62377e0c69a48c04c8b2382ef27c5447c50ca7f1
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2014-06-05 11:27:25 -0400

    Add employer page.

--- src/routes.php
+++ src/routes.php
@@ -4,6 +4,10 @@ return function(\Slim\Slim $app) {
         $app->render('home.html', ['page' => 'home']);
     })->name('home');
 
+    $app->get('/employer', function() use($app) {
+        $app->render('employer.html', ['page' => 'employer']);
+    })->name('employer');
+
     $app->get('/video', function() use($app) {
         $app->render('video.html', ['page' => 'video']);
     })->name('video');
