20fbb0edf998d3448fd4a88d949cc28b9a2870cd
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2014-06-05 19:49:21 -0400

    Format routes.php again.

--- src/routes.php
+++ src/routes.php
@@ -3,18 +3,23 @@ return function(\Slim\Slim $app) {
     $app->get('/', function() use($app) {
         $app->render('home.html', ['page' => 'home']);
     })->name('home');
+
     $app->get('/employer', function() use($app) {
         $app->render('employer.html', ['page' => 'employer']);
     })->name('employer');
+
     $app->get('/video', function() use($app) {
         $app->render('video.html', ['page' => 'video']);
     })->name('video');
+
     $app->get('/form', function() use($app) {
         $app->render('form.html', ['page' => 'form']);
     })->name('form');
+
     $app->get('/share', function() use($app) {
         $app->render('share.html', ['page' => 'share']);
     })->name('share');
+
     $app->post('/submit', function() use($app) {
         $app->contentType('application/json');
         $tmpDir = uniqid();
