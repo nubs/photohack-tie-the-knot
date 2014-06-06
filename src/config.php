7734ddc19ac80e51c6f65f3641343a57edaa185e
Robert Bittle <guywithnose@gmail.com>
2014-06-06 08:34:31 -0400

    Set up youtube

--- src/config.php
+++ src/config.php
@@ -9,13 +9,21 @@ return function($app) {
     $view = $app->view($twigView);
     $view->parserExtensions = [new \Slim\Views\TwigExtension()];
 
-    $app->container->singleton('youtube', function() {
+    $app->container->singleton('youtube', function() use($app) {
+        return new Youtube($app->client);
+    });
+
+    $app->container->singleton('client', function() use($app) {
         $client = new Google_Client();
         $client->setClientId(getenv('YOUTUBE_CLIENT_ID'));
         $client->setClientSecret(getenv('YOUTUBE_CLIENT_SECRET'));
         $client->setScopes('https://www.googleapis.com/auth/youtube');
-        $client->setRedirectUri('http://photohack.herokuapp.com/share');
+        $client->setRedirectUri('http://local.photohack.com/share');
+        $token = $app->getCookie('token');
+        if ($token) {
+            $client->setAccessToken($token);
+        }
 
-        return new Youtube($client);
+        return $client;
     });
 };
